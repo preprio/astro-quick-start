7d98de0872bdfe508e762b40d117c77bd5c9082e
Michiel Meeuwissen <git@meeuw.org>
2023-06-20 12:14:35 +0200

    Started on a simple migration script, just to see how that would work.

--- migrate-test/3voor12-updates.py
+++ migrate-test/3voor12-updates.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python3
+import os
+
+import elasticsearch
+
+
+from datetime import datetime
+from elasticsearch import Elasticsearch, helpers
+from dotenv import dotenv_values
+
+
+
+config = dotenv_values(os.path.dirname(os.path.dirname(__file__)) + '/.env')
+
+es = Elasticsearch("http://localhost:9210")
+
+resp = helpers.scan(
+    es,
+    index="3voor12_updates", 
+    scroll = '3m'
+)
+
+prepr_endpoint = config['PREPR_MIGRATE_ENDPOINT']
+
+for num, doc in enumerate(resp):    
+    print("%s" % str(doc))
+    # TODO: Now post the update to prepr to create a bunch of content items
 
