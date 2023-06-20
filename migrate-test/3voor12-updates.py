55aa6290b6d7a68467bd5ec42167b0da6ea2ebd7
Michiel Meeuwissen <git@meeuw.org>
2023-06-20 13:37:38 +0200

    More remarks and findings.

--- migrate-test/3voor12-updates.py
+++ migrate-test/3voor12-updates.py
@@ -1,28 +1,76 @@
 #!/usr/bin/env python3
 import os
 
-import elasticsearch
-
-
-from datetime import datetime
+import requests
 from elasticsearch import Elasticsearch, helpers
 from dotenv import dotenv_values
 
 
-
 config = dotenv_values(os.path.dirname(os.path.dirname(__file__)) + '/.env')
+def post_to_prepr(fields):
+    token = config['PREPR_MIGRATE_TOKEN']
+    response = requests.post("https://api.eu1.prepr.io/content_items",
+       headers={
+              'Authorization':  'Bearer ' + token
+       },
+       json={
+           "locales": [ "nl-NL"],
+           "model": {
+             "id": "e1cc318d-6b70-413d-8797-b438734fd19d"
+           },
+           "status": {
+              "nl-NL": {
+                "body": "Done"
+             }
+           },
+          "publish_on": {
+             "nl-NL": 1600262640
+           },
+           "items": {
+               "nl-NL": {        
+                   "title": {
+                      "body": fields['title']
+                  },
+                  "mgnl_uuid": {
+                      "body": fields['id'][0:20]
+                  }
+               }
+           }
+       }
+    )
+    if response.status_code == 201:
+        print(response.status_code)
+        print(response.json())
+        return
+    else: 
+        raise Exception("Error posting to Prepr: " + str(response.status_code) + " " + response.text)
 
-es = Elasticsearch("http://localhost:9210")
 
-resp = helpers.scan(
-    es,
-    index="3voor12_updates", 
-    scroll = '3m'
-)
 
-prepr_endpoint = config['PREPR_MIGRATE_ENDPOINT']
+def map_to_prepr(source):
+    return {
+        'id': source['id'],
+        'title': source['title'], 
+        'text': source['text']
+    }
 
-for num, doc in enumerate(resp):    
-    print("%s" % str(doc))
-    # TODO: Now post the update to prepr to create a bunch of content items
+def migrate():
+    es = Elasticsearch("http://localhost:9210")
 
+    resp = helpers.scan(
+      es,
+      index="3voor12_updates", 
+      scroll = '3m'
+    )
+
+
+
+    for num, doc in enumerate(resp):    
+        print("%s" % str(doc))
+        source = doc["_source"]    
+        post_to_prepr(map_to_prepr(source))            
+
+
+
+
+migrate()
